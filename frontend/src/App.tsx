72f83f6e164149b7a7e460017cec77322692bf69
Daniel Oliveira <danielsapup3@gmail.com>
2021-05-04 21:07:12 -0400

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import NavBar from "components/NavBar";
+import DataTable from 'components/DataTable';
+import Footer from "components/Footer";
+
 function App() {
   return (
-    <div>
+    <>
+    <NavBar />
+    <div className="container">
       <h1 className="text-primary">Olá, Mundo!</h1>
+      <DataTable />
     </div>
+    <Footer />
+    </>
   );
 }
 
